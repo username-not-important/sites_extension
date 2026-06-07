c0ef27cafe7b464f7bd098cbaf00c63e9403037f
username-not-important <ac_ali2582@yahoo.com>
2026-06-07 14:14:46 +0330

    init

---
+++ content.js
@@ -0,0 +1,7 @@
+// Your custom JavaScript will go here
+console.log("AvalAI Chat Enhancer loaded!");
+
+// Example: You can add code here
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("Page loaded, enhancements active");
+});
